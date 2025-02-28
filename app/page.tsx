547c15bd5e09dca8aa7beef8f224797b6bf11338
FOrhadul Islam <mdforhadul44@gmail.com>
2025-02-28 23:48:45 +0600

    :add allll:

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,21 @@
 import HomeBanarSection from "@/components/Home/HomeBanarSection";
 import HomeCustomerReview from "@/components/Home/HomeCustomerReview";
 import HomeHeroSection from "@/components/Home/HomeHeroSection";
+import Head from "next/head";
 // import HomeSubscriptionSection from "@/components/Home/HomeSubscriptionSection";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta property="og:logo" content="https://www.finex.ltd/logo-en.png" />
+        <meta property="og:finex" content="https://www.finex.ltd/" />
+        <meta property="og:Finex - Home" content="Link preview title" />
+        <meta
+          property="og:description"
+          content="Finex provides fast, reliable, and affordable international shipping solutions, specializing in air and sea freight, custom clearance, and competitive pricing."
+        />
+      </Head>
       <HomeHeroSection />
       <HomeBanarSection />
       <HomeCustomerReview />
