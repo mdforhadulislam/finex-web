3213b4b2d3a8478e2cd34bc76fe13290773ef321
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2025-04-26 01:22:53 +0600

    Update GoogleAnalytics.tsx

--- utils/GoogleAnalytics.tsx
+++ utils/GoogleAnalytics.tsx
@@ -3,6 +3,12 @@
 import Script from 'next/script';
 import { useEffect } from 'react';
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 export default function GoogleAnalytics() {
   useEffect(() => {
     window.dataLayer = window.dataLayer || [];
@@ -14,7 +20,7 @@ export default function GoogleAnalytics() {
   }, []);
 
   return (
-    <>
+<>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-SWC7MTSL7V"
         strategy="afterInteractive"
