9e4f238239c83e7a4e33bbef011b646a6d663075
FOrhadul Islam <mdforhadul44@gmail.com>
2025-06-26 15:16:09 +0600

    addd alll

--- components/ApiCall/ApiMethod.js
+++ components/ApiCall/ApiMethod.js
@@ -1,11 +1,15 @@
 // export const ROOT_API = `http://localhost:5000/`;
-export const ROOT_API = `https://finex.up.railway.app/`;
-// export const ROOT_API = `https://finex-server.onrender.com/`;
+// export const ROOT_API = `https://finex.up.railway.app/`;
+export const ROOT_API = `https://finex-server.onrender.com/`;
 
 export const ROOT_API_V1 = `${ROOT_API}api/v1/`;
-export const LOGIN_API = `${ROOT_API_V1}auth/login/`
-export const REGISTER_API = `${ROOT_API_V1}auth/register/`
-export const LOGOUT_API = `${ROOT_API_V1}auth/logout/`
+
+
+export const LOGIN_API = `https://finex.up.railway.app/auth/login/`
+export const REGISTER_API = `https://finex.up.railway.app/auth/register/`
+export const LOGOUT_API = `https://finex.up.railway.app/auth/logout/`
+
+
 export const USER_ACCOUNT_API = (phone)=> `${ROOT_API_V1}account/${phone}`
 export const PUBLIC_TRACKING_API = (id) => `${ROOT_API_V1}track/${id}`;
 export const VISITOR_POST_API = `${ROOT_API_V1}utils/visitor/`
