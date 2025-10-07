4e6c0b9352858b915eee267c9f7e9cec6c10a31b
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2025-10-07 17:56:28 +0600

    Update HomeBanarSection.tsx

--- components/Home/HomeBanarSection.tsx
+++ components/Home/HomeBanarSection.tsx
@@ -12,22 +12,11 @@ import { useEffect, useState } from "react";
 
 const slider_image = [
   { id: 135, src: "/slider-1.jpg" },
-  { id: 2345, src: "/banar2.png" },
   { id: 123, src: "/slider-1.jpg" },
-  { id: 234, src: "/banar2.png" },
   { id: 156, src: "/slider-1.jpg" },
-  { id: 278, src: "/banar2.png" },
-  
   { id: 1535, src: "/slider-1.jpg" },
-  { id: 267, src: "/banar2.png" },
   { id: 165, src: "/slider-1.jpg" },
-  { id: 243, src: "/banar2.png" },
   { id: 121, src: "/slider-1.jpg" },
-  { id: 23456, src: "/banar2.png" },
-
-
-
-
 ];
 
 const HomeBanarSection = () => {
@@ -44,14 +33,14 @@ const HomeBanarSection = () => {
   }, [api]);
 
   return (
-    <div className="w-full h-auto py-16 pb-0 bg-white px-5">
-      <div className="container h-auto m-auto p-10">
+    <div className="w-full h-auto py-16 pb-0 bg-white px-3">
+      <div className="container h-auto m-auto p-5">
         <Carousel setApi={setApi}>
           <CarouselContent>
             {slider_image.map((item) => (
               <CarouselItem key={item.id}>
                 <div
-                  className="w-full h-[230px] md:h-[480px] p-2 bg-cover rounded-lg bg-no-repeat bg-center"
+                  className="w-full h-[260px] md:h-[500px] p-2 bg-cover rounded-lg bg-no-repeat bg-center"
                   style={{ backgroundImage: `url(${item.src})` }}
                 ></div>
               </CarouselItem>
