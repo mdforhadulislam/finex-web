f52b33ee38d7cdbc5e397e2ad75151b898c64d45
FOrhadul Islam <mdforhadul44@gmail.com>
2025-10-07 18:10:43 +0600

    add alll

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,25 @@
+import WhyFinexBodySection from "@/components/About/WhyFinexBodySection";
+import WorkProcessBodySection from "@/components/About/WorkProcessBodySection";
+import OurBlogBodySection from "@/components/Blog/OurBlogBodySection";
 import HomeBanarSection from "@/components/Home/HomeBanarSection";
 import HomeCustomerReview from "@/components/Home/HomeCustomerReview";
 import HomeHeroSection from "@/components/Home/HomeHeroSection";
 import PriceCalCulatorSection from "@/components/Price/PriceCalCulatorSection";
-import Head from "next/head";
 // import HomeSubscriptionSection from "@/components/Home/HomeSubscriptionSection";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta property="og:logo" content="https://www.finex.ltd/logo-en-.png" />
-        <meta property="og:finex" content="https://www.finex.ltd/" />
-        <meta property="og:Finex - Home" content="Link preview title" />
-        <meta
-          property="og:description"
-          content="Finex provides fast, reliable, and affordable international shipping solutions, specializing in air and sea freight, custom clearance, and competitive pricing."
-        />
-      </Head>
       <HomeHeroSection />
       <HomeBanarSection />
-      <div  className=" w-full p-2 sm:container h-auto m-auto">
-      <PriceCalCulatorSection />
+      <div className=" w-full p-2 sm:container h-auto m-auto">
+        <WhyFinexBodySection />
+        <PriceCalCulatorSection />
+
+        <WorkProcessBodySection />
+        <OurBlogBodySection />
       </div>
+
       <HomeCustomerReview />
     </>
   );
