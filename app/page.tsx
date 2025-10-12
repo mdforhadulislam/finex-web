e1c79e3a27744e7d81f4108a3ff1627cda086225
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2025-10-13 00:47:12 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import OurTeamMemberBodySection from "@/components/About/OurTeamMemberBodySection";
-import WhyFinexBodySection from "@/components/About/WhyFinexBodySection";
-import WorkProcessBodySection from "@/components/About/WorkProcessBodySection";
-import OurBlogBodySection from "@/components/Blog/OurBlogBodySection";
+// import OurTeamMemberBodySection from "@/components/About/OurTeamMemberBodySection";
+// import WhyFinexBodySection from "@/components/About/WhyFinexBodySection";
+// import WorkProcessBodySection from "@/components/About/WorkProcessBodySection";
+// import OurBlogBodySection from "@/components/Blog/OurBlogBodySection";
 import HomeBanarSection from "@/components/Home/HomeBanarSection";
 import HomeCustomerReview from "@/components/Home/HomeCustomerReview";
 import HomeHeroSection from "@/components/Home/HomeHeroSection";
